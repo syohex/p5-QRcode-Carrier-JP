b03104b2439e63fde4830a890bf903bf48e9f3a7
Syohei YOSHIDA <syohex@gmail.com>
2011-03-16 21:50:48 +0900

    add function which translate contact information to string

--- examples/qrcode_html.pl
+++ examples/qrcode_html.pl
@@ -28,9 +28,11 @@ if ($@) {
 my $qrcode_info = $app->run($contact_info);
 
 my $png_base64 = {};
-for my $name (%{$qrcode_info}) {
+my $info_string = {};
+for my $name (keys %{$qrcode_info}) {
     my $qrcode_imager = $qrcode_info->{$name};
 
+    $info_string->{$name} = contact_info_as_string($contact_info->{$name});
     for my $carrier (sort keys %{$qrcode_imager}) {
         my $png_data;
         $qrcode_imager->{$carrier}->write(
@@ -38,7 +40,7 @@ for my $name (%{$qrcode_info}) {
             type => 'png',
         );
 
-        push @{$png_base64->{$name}}, encode_base64($png_data);
+        $png_base64->{$name}->{$carrier} = encode_base64($png_data);
     }
 }
 
@@ -56,10 +58,39 @@ my $output_str = $tx->render_string(
     $tmpl_str,
     {
         png_base64 => $png_base64,
+        info_string => $info_string,
     },
 );
 
 print encode_utf8($output_str);
+
+sub contact_info_as_string {
+    my $contact_info = shift;
+
+    my @attrs;
+
+    push @attrs, "名前:";
+    push @attrs, "<strong>" . $contact_info->{name1} .
+        "(" . $contact_info->{name2} . ")</strong>";
+
+    push @attrs, "メールアドレス:";
+    push @attrs, $_ for @{$contact_info->{mail_addresses}};
+
+    push @attrs, "電話番号:";
+    push @attrs, $_ for @{$contact_info->{telephones}};
+
+    if ($contact_info->{address}) {
+        push @attrs, "住所:";
+        push @attrs, $contact_info->{address};
+    }
+
+    if ($contact_info->{memory}) {
+        push @attrs, "メモ:";
+        push @attrs, $contact_info->{memory};
+    }
+
+    return join "<br />", @attrs;
+}
 
 __DATA__
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
@@ -70,41 +101,27 @@ __DATA__
 <meta http-equiv="Content-Style-Type" content="text/css" />
 <title>キャリア別 QRコード</title>
 <style type="text/css">
-
 div.main {
-  width: 800px;
+  width: 900px;
   margin: 0 auto 0 auto;
   text-align:center;
 }
-
 table, td, th {
   border: 2px #808080 solid;
 }
-
 table {
   margin: 0 auto 0 auto;
 }
-
 th {
   font-family: sans-serif;
   font-size: 14px;
 }
-
-th.name-section {
-  width: 100px;
+td.contact_info {
   font-family: sans-serif;
-  font-size: 14px;
-}
-
-td.name-section {
-  width: 100px;
-  font-family: sans-serif;
-  font-size: 14px;
-  font-weight: bold;
-  text-align: center;
+  font-size: 12px;
+  text-align: left;
 }
 </style>
-
 </head>
 
 <body>
@@ -112,7 +129,7 @@ td.name-section {
 <h1>キャリア別 QRコード</h1>
 <table>
 <tr>
-  <th class="name-section">名前</th>
+  <th>連絡情報</th>
   <th>au</th>
   <th>docomo</th>
   <th>softbank</th>
@@ -120,10 +137,11 @@ td.name-section {
 
 : for $png_base64.keys() -> $name {
   <tr>
-  <td class="name-section"><: $name :></td>
-:  for $png_base64[$name] -> $carrier_data {
+  <td class="contact_info"><: $info_string[$name] | mark_raw :></td>
+:  for $png_base64[$name].keys().sort() -> $carrier {
       <td>
-         <img src="data:image/png;base64,<: $carrier_data :>" />
+         <img src="data:image/png;base64,<: $png_base64[$name][$carrier] :>"
+              alt="<: $carrier :>_QRcode" />
       </td>
 :  }
   </tr>
