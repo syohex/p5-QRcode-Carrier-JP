055caddd6cec194fd55e324810e86b10605aff92
Syohei YOSHIDA <syohex@gmail.com>
2011-03-18 00:49:34 +0900

    Add class QRCode::Carrier::JP::Contact::Info

--- lib/QRCode/Carrier/JP.pm
+++ lib/QRCode/Carrier/JP.pm
@@ -3,277 +3,45 @@ use strict;
 use warnings;
 
 use 5.008_001;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp ();
 use Encode ();
-use Encode::JP::H2Z ();
-use File::Spec ();
-use Imager::QRCode;
 
+use QRCode::Carrier::JP::ContactInfo;
+
+use constant DEBUG => defined $ENV{QRCODE_DEBUG};
 our $ENCODE_CHARSET = 'utf-8';
-
-my @carriers = qw/docomo au softbank/;
-my %qr_func = (
-    docomo   => \&_create_docomo_data,
-    au       => \&_create_au_data,
-    softbank => \&_create_softbank_data,
-);
-
-my %valid_pattern = (
-    mail_addresses => qr/^[^@]+\@[^@]+$/xms,
-    telephones     => qr/^[0-9#P\-]+$/xms,
-);
-
-my %limit_length = (
-    memory         => 80,
-    name1          => 24,
-    name2          => 24,
-    mail_addresses => 60,
-    telephones     => 24,
-    address        => 80,
-);
 
 sub new {
     my ($class, $args) = @_;
-
-    my $attrs = {};
-
-    $attrs->{debug}    = delete $args->{debug} || 0;
-    $attrs->{qrcode} = Imager::QRCode->new(
-        version       => 1,
-        level         => 'M',
-        casesensitive => 1,
-        lightcolor    => Imager::Color->new(255, 255, 255),
-        darkcolor     => Imager::Color->new(0, 0, 0),
-    );
-
-    bless $attrs, $class;
+    bless {}, $class;
 }
 
 sub run {
-    my ($self, $contact_info) = @_;
+    my ($self, $args) = @_;
 
-    unless (ref $contact_info eq "HASH") {
-        Carp::croak("contact_info parameter must be HASH reference\n");
+    unless (ref $args eq "HASH") {
+        Carp::croak("Argument must be HASH reference\n");
     }
 
-    $self->_validate_contact_info($contact_info);
+    my @contact_infos;
+    while (my ($id, $info) = each %{$args}) {
+        $info->{id} = $id;
 
-    my $retval = {};
-    while ( my ($name, $info) = each %{$contact_info} ) {
-        for my $carrier (@carriers) {
-            my $data = $qr_func{$carrier}->($self, $info);
-            if ($self->{debug} > 2) {
-                my $decoded = Encode::decode('shift_jis', $data);
-                $decoded =~ s{\r\n$}{\n}xmsg;
-                printf STDERR "\n%s\n",
-                    Encode::encode($ENCODE_CHARSET, $decoded);
-            }
+        my $contact_info = QRCode::Carrier::JP::ContactInfo->new($info);
 
-            my $output = File::Spec->catfile(
-                $self->{output_dir}, $carrier, "${name}.png"
-            );
-
-            my $imager = $self->{qrcode}->plot($data);
-
-            $retval->{$name}->{$carrier} = $imager;
+        if (DEBUG) {
+            Carp::carp("Validate ", Encode::encode($ENCODE_CHARSET, $id), "\n");
         }
+        $contact_info->validate;
+
+        $contact_info->create_qr_code();
+
+        push @contact_infos, $contact_info;
     }
 
-    $retval;
-}
-
-sub _validate_contact_info {
-    my ($self, $contact_info) = @_;
-
-    while ( my ($name, $info) = each %{$contact_info} ) {
-        $self->_validate_contact_info_each($name, $info);
-    }
-}
-
-sub _validate_contact_info_each {
-    my ($self, $name, $info) = @_;
-
-    my $encoded_name = Encode::encode($ENCODE_CHARSET, "$name");
-
-    if ($self->{debug}) {
-        printf STDERR "Validate %s's contact information.... ", $encoded_name;
-    }
-
-    ## mandatory parameters
-    unless (exists $info->{name1}) {
-        Carp::croak("Not specified 'name1' parameter\n");
-    }
-
-    unless ($info->{name1} =~ m{\w+ \s \w+}xms) {
-        Carp::croak("name1 parameter."
-           , "You must separate family name and first name with a space\n");
-    }
-
-    # '+ 1' means a space which separates family name and first name
-    _check_byte_length($info->{name1}, $limit_length{name1} + 1);
-
-    unless (exists $info->{name2}) {
-        Carp::croak("Not specified 'name2' parameter\n");
-    }
-
-    unless ($info->{name2} =~ m{(\w+) \s (\w+)}xms) {
-        Carp::croak("name2 parameter."
-           , "You must separate family name and first name with a space\n");
-    }
-
-    # '+ 1' means a space which separates family name and first name
-    _check_byte_length($info->{name1}, $limit_length{name2});
-
-    my ($family, $first) = ($1, $2);
-    my $katakana = Encode::decode_utf8('ァ-ン');
-    my $katakana_regexp = qr{^[$katakana]+$}xms;
-    for my $part ($family, $first) {
-        unless ($part =~ m{$katakana_regexp}) {
-            Carp::croak("name2 parameter must consist of KATAKANA, $part\n");
-        }
-    }
-
-    for my $parameter (qw/mail_addresses telephones/) {
-        unless (exists $info->{$parameter}) {
-            Carp::croak("$parameter parameter is mandatory parameter.\n");
-        }
-
-        unless (ref $info->{$parameter} eq 'ARRAY') {
-            Carp::croak("$parameter parameter must be ARRAY ref.\n");
-        }
-
-        if (scalar @{$info->{$parameter}} > 3) {
-            Carp::croak("$parameter too long. It must be less than equal 3.\n");
-        }
-
-        _check_byte_length($info->{name1}, $limit_length{$parameter});
-
-        my $regexp = $valid_pattern{$parameter};
-        for my $val (@{$info->{$parameter}}) {
-            unless ($val =~ m{$regexp}) {
-                Carp::croak("$parameter invalid: $val\n");
-            }
-        }
-    }
-
-    ## optional parameters
-    for my $key (qw/memory address/) {
-        if (defined $info->{$key}) {
-            _check_byte_length($info->{$key}, $limit_length{$key});
-        } else {
-            $info->{$key} = '';
-        }
-    }
-
-    if ($self->{debug}) {
-        printf STDERR "OK\n", $encoded_name;
-    }
-}
-
-sub _create_docomo_data {
-    my ($self, $info) = @_;
-
-    my @params;
-
-    my ($family_name, $first_name) = split /\s/, $info->{name1};
-    my $name_param = join ',',  $family_name, $first_name;
-    push @params, "MECARD:N:" . Encode::encode('shift_jis', $name_param);
-
-    my ($family_kana, $first_kana) = map {
-        _hankaku_to_zenkaku($_)
-    } split /\s/, $info->{name2};
-    my $sound_param = join ",", $family_kana, $first_kana;
-    push @params, "SOUND:" . Encode::encode('shift_jis', $sound_param);
-
-    for my $telephone (@{$info->{telephones}}) {
-        $telephone =~ s{-}{}g;
-        push @params, "TEL:" . $telephone;
-    }
-
-    for my $email (@{$info->{mail_addresses}}) {
-        push @params, "EMAIL:" . $email;
-    }
-
-    my $note = $info->{memory};
-    push @params, "NOTE:", Encode::encode('shift_jis', $note);
-
-    my $data = join ';', @params;
-    $data .= ';;';
-
-    $data;
-}
-
-sub _create_au_data {
-    my ($self, $info) = @_;
-
-    # Specification of au is based on softbank one.
-    my $data = $self->_create_softbank_data($info);
-
-    # ADD(address)
-    $data .= "ADD:" . Encode::encode('shift_jis', $info->{address});
-    $data .= "\015\012";
-
-    $data;
-}
-
-sub _create_softbank_data {
-    my ($self, $info) = @_;
-    my @params;
-
-    my $memory = defined $info->{memory} ? $info->{memory} : '';
-    push @params, "MEMORY:" . Encode::encode('shift_jis', $memory);
-
-    (my $name1 = $info->{name1}) =~ s{\s}{}xms;
-    push @params, "NAME1:" . Encode::encode('shift_jis', $name1);
-
-    (my $name2 = $info->{name2}) =~ s{\s}{}xms;
-    my $hankaku_name2 = _hankaku_to_zenkaku($name2);
-    push @params, "NAME2:" . Encode::encode('shift_jis', $hankaku_name2);
-
-    {
-        my $i = 1;
-        for my $mail_address (@{$info->{mail_addresses}}) {
-            push @params, "MAIL${i}:" . $mail_address;
-            $i++;
-        }
-    }
-
-    {
-        my $i = 1;
-        for my $telephone (@{$info->{telephones}}) {
-            $telephone =~ s{-}{}g;
-            push @params, "TEL${i}:" . $telephone;
-            $i++;
-        }
-    }
-
-    my $data = join "\015\012", @params;
-
-    $data .= "\015\012";
-    $data;
-}
-
-sub _hankaku_to_zenkaku {
-    my $zenkaku_str = shift;
-
-    my $euc_str = Encode::encode('euc-jp', $zenkaku_str);
-    Encode::JP::H2Z::z2h(\$euc_str);
-
-    return Encode::decode('euc-jp', $euc_str);
-}
-
-sub _check_byte_length {
-    my ($string, $limit) = @_;
-
-    {
-        use bytes;
-
-        if (length $string > $limit) {
-            Carp::croak("$string is too long\n");
-        }
-    }
+    return @contact_infos;
 }
 
 1;
---
+++ lib/QRCode/Carrier/JP/ContactInfo.pm
@@ -0,0 +1,358 @@
+package QRCode::Carrier::JP::ContactInfo;
+use strict;
+use warnings;
+
+use Carp ();
+use Imager::QRCode;
+
+use utf8;
+use Encode ();
+use Lingua::JA::Regular::Unicode ();
+
+use MIME::Base64 ();
+use URI::Escape ();
+
+use Class::Accessor::Lite (
+    rw => [
+        qw/id memory name1 name2
+           mail_addresses telephones address qrcode /
+       ],
+);
+
+my @CARRIERS = qw/docomo au softbank/;
+
+my %valid_pattern = (
+    mail_addresses => qr/^[^@]+\@[^@]+$/xms,
+    telephones     => qr/^[0-9#P\-]+$/xms,
+);
+
+my %limit_length = (
+    memory         => 80,
+    name1          => 24,
+    name2          => 24,
+    mail_addresses => 60,
+    telephones     => 24,
+    address        => 80,
+);
+
+sub new {
+    my ($class, $args) = @_;
+
+    my $obj = bless {}, $class;
+
+    $obj->id($args->{id});
+
+    unless (defined $args->{name1}) {
+        Carp::croak("Not specified 'name1' parameter\n");
+    }
+
+    $obj->name1($args->{name1});
+
+    unless (exists $args->{name2}) {
+        Carp::croak("Not specified 'name2' parameter\n");
+    }
+
+    $obj->name2($args->{name2});
+
+    for my $param (qw/mail_addresses telephones/) {
+        unless (ref $args->{$param} eq 'ARRAY' && scalar @{$args->{$param}} > 0) {
+            Carp::croak($obj->id, ":$param paramter must be ARRAY reference"
+                            , " and its length must be grater than 0\n");
+        }
+
+        $obj->$param($args->{$param});
+    }
+
+    for my $param (qw/memory address/) {
+        my $val = defined $args->{$param} ? $args->{$param} : '';
+        $obj->$param($val);
+    }
+
+    return $obj;
+}
+
+sub validate {
+    my $self = shift;
+
+    my $name1 = $self->name1;
+    unless ($name1 =~ m{\w+ \s \w+}xms) {
+        Carp::croak("name1 parameter."
+           , "You must separate family name and first name with a space\n");
+    }
+
+    # '+ 1' means a space which separates family name and first name
+    my $shift_jis_name1= Encode::encode('shift_jis', $name1);
+    _check_byte_length($shift_jis_name1, $limit_length{name1} + 1);
+
+    my $name2 = $self->name2;
+
+    unless ($name2 =~ m{(\w+) \s (\w+)}xms) {
+        Carp::croak("name2 parameter."
+           , "You must separate family name and first name with a space\n");
+    }
+
+    my ($family, $first) = ($1, $2);
+
+    # '+ 1' means a space which separates family name and first name
+    my $shift_jis_name2 = Encode::encode('shift_jis', $name2);
+    _check_byte_length($shift_jis_name2, $limit_length{name2} + 1);
+
+    my $katakana = Encode::decode_utf8('ァ-ン');
+    my $katakana_regexp = qr{^[$katakana]+$}xms;
+    for my $part ($family, $first) {
+        unless ($part =~ m{$katakana_regexp}) {
+            Carp::croak("name2 parameter must consist of KATAKANA, $part\n");
+        }
+    }
+
+    for my $parameter (qw/mail_addresses telephones/) {
+        my $array_ref = $self->$parameter;
+
+        if (scalar @{$array_ref} > 3) {
+            Carp::croak("$parameter too long. It must be less than equal 3.\n");
+        }
+
+        my $regexp = $valid_pattern{$parameter};
+        for my $element (@{$array_ref}) {
+            unless ($element =~ m{$regexp}) {
+                Carp::croak("$parameter invalid: $element\n");
+            }
+
+            _check_byte_length($element, $limit_length{$parameter});
+        }
+    }
+
+    ## optional parameters
+    for my $key (qw/memory address/) {
+        my $shift_jis_val = Encode::encode('shift_jis', $self->$key);
+        _check_byte_length($shift_jis_val, $limit_length{$key});
+    }
+}
+
+sub qrcode_as_base64 {
+    my ($self, $carrier) = @_;
+
+    _check_carrier($carrier);
+
+    my $qrcode = Imager::QRCode->new(
+        version       => 1,
+        level         => 'M',
+        casesensitive => 1,
+        lightcolor    => Imager::Color->new(255, 255, 255),
+        darkcolor     => Imager::Color->new(0, 0, 0),
+    );
+
+    my $imager = $qrcode->plot( $self->qrcode->{$carrier} );
+    $imager->write(
+        data => \my $png_data,
+        type => 'png',
+    );
+
+    return MIME::Base64::encode_base64($png_data);
+}
+
+sub output_qrcode {
+    my ($self, $carrier, $output) = @_;
+
+    _check_carrier($carrier);
+
+    my $qrcode = Imager::QRCode->new(
+        version       => 1,
+        level         => 'M',
+        casesensitive => 1,
+        lightcolor    => Imager::Color->new(255, 255, 255),
+        darkcolor     => Imager::Color->new(0, 0, 0),
+    );
+
+    my $imager = $qrcode->plot( $self->qrcode->{$carrier} );
+    $imager->write(
+        file => $output,
+    );
+}
+
+sub _check_carrier {
+    my $carrier = shift;
+
+    unless (defined $carrier) {
+        Carp::croak("Error not specify carrier(@CARRIERS)\n");
+    }
+
+    unless (grep { $carrier eq $_ } @CARRIERS) {
+        Carp::croak("Error invalid carrier $carrier\n");
+    }
+}
+
+sub create_qr_code {
+    my $self = shift;
+
+    my %qrdata_generator = (
+        docomo   => \&_create_docomo_data,
+        au       => \&_create_au_data,
+        softbank => \&_create_softbank_data,
+    );
+
+    my %imager_qrcode;
+    for my $carrier (@CARRIERS) {
+        $imager_qrcode{$carrier} = $qrdata_generator{$carrier}->($self);
+    }
+
+    $self->qrcode(\%imager_qrcode);
+}
+
+sub _create_docomo_data {
+    my $self = shift;
+
+    my @params;
+
+    my ($family_name, $first_name) = split /\s/, $self->name1;
+    my $name_param = join ',',  $family_name, $first_name;
+    push @params, "MECARD:N:" . Encode::encode('shift_jis', $name_param);
+
+    my ($family_kana, $first_kana) = map {
+        Lingua::JA::Regular::Unicode::katakana_z2h($_);
+    } split /\s/, $self->name2;
+
+    my $sound_param = join ",", $family_kana, $first_kana;
+    push @params, "SOUND:" . Encode::encode('shift_jis', $sound_param);
+
+    for my $telephone (@{$self->telephones}) {
+        $telephone =~ s{-}{}g;
+        push @params, "TEL:" . $telephone;
+    }
+
+    for my $email (@{$self->mail_addresses}) {
+        push @params, "EMAIL:" . $email;
+    }
+
+    my $note = $self->memory;
+    push @params, "NOTE:", Encode::encode('shift_jis', $note);
+
+    my $data = join ';', @params;
+    $data .= ';;';
+
+    $data;
+}
+
+sub _create_au_data {
+    my $self = shift;
+
+    # Specification of au is based on softbank one.
+    my $data = $self->_create_softbank_data();
+
+    # ADD(address)
+    $data .= "ADD:" . Encode::encode('shift_jis', $self->address);
+    $data .= "\015\012";
+
+    $data;
+}
+
+sub _create_softbank_data {
+    my $self = shift;
+    my @params;
+
+    push @params, "MEMORY:" . Encode::encode('shift_jis', $self->memory);
+
+    (my $name1 = $self->name1) =~ s{\s}{}xms;
+    push @params, "NAME1:" . Encode::encode('shift_jis', $name1);
+
+    (my $name2 = $self->name2) =~ s{\s}{}xms;
+    my $hankaku_name2 = Lingua::JA::Regular::Unicode::katakana_z2h($name2);
+    push @params, "NAME2:" . Encode::encode('shift_jis', $hankaku_name2);
+
+    {
+        my $i = 1;
+        for my $mail_address (@{$self->mail_addresses}) {
+            push @params, "MAIL${i}:" . $mail_address;
+            $i++;
+        }
+    }
+
+    {
+        my $i = 1;
+        for my $telephone (@{$self->telephones}) {
+            $telephone =~ s{-}{}g;
+            push @params, "TEL${i}:" . $telephone;
+            $i++;
+        }
+    }
+
+    my $data = join "\015\012", @params;
+
+    $data .= "\015\012";
+    $data;
+}
+
+sub _check_byte_length {
+    my ($string, $limit) = @_;
+
+    {
+        use bytes;
+
+        if (length $string > $limit) {
+            Carp::croak("$string is too long\n");
+        }
+    }
+}
+
+sub as_string {
+    my $self = shift;
+
+    my @attrs;
+
+    push @attrs, "名前:";
+    push @attrs, $self->name1 . "(" . $self->name2 . ")";
+
+    push @attrs, "メールアドレス:";
+    push @attrs, $_ for @{$self->mail_addresses};
+
+    push @attrs, "電話番号:";
+    push @attrs, $_ for @{$self->telephones};
+
+    if ($self->address) {
+        push @attrs, "住所:";
+        push @attrs, $self->address;
+    }
+
+    if ($self->memory) {
+        push @attrs, "メモ:";
+        push @attrs, $self->memory;
+    }
+
+    return join "\n", @attrs;
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+QRCode::Carrier::JP::ContactInfo - Contact information
+
+=head1 SYNOPSIS
+
+  use QRCode::Carrier::JP::ContactInfo;
+
+=head1 DESCRIPTION
+
+QRCode::Carrier::JP::ContactInfo is
+
+=head1 INTERFACE
+
+=head1 AUTHOR
+
+Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+C<Imager::QRCode>
+
+=head1 LICENSE
+
+Copyright 2011- Syohei YOSHIDA
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
