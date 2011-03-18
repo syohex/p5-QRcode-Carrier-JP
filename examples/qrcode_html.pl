911b4956e37122b4e3542ae2e84795b45b1a73f0
Syohei YOSHIDA <syohex@gmail.com>
2011-03-18 14:49:34 +0900

    Use template file instead of __DATA__ section in example program.

--- examples/qrcode_html.pl
+++ examples/qrcode_html.pl
@@ -26,67 +26,11 @@ if ($@) {
 
 my @contact_infos = $app->run($contact_info);
 
-my $tmpl_str = do {
-    local $/;
-    decode_utf8(scalar <DATA>);
-};
-
 my $tx = Text::Xslate->new(
-    function => {
-        newline_to_br_tag => sub {
-            my $str = shift;
-            $str =~ s{\n}{<br />}gxms;
-            $str;
-        },
-    },
+    syntax => 'Kolon',
 );
-my $output_str = $tx->render_string($tmpl_str, {
+
+my $output_str = $tx->render('sample.tx', {
     contact_infos => \@contact_infos,
-    carriers      => [ qw/au docomo softbank/ ],
 });
 print encode_utf8($output_str);
-
-__DATA__
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-   "http://www.w3.org/TR/html4/strict.dtd">
-<html lang="ja">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta http-equiv="Content-Style-Type" content="text/css" />
-<title>キャリア別 QRコード</title>
-<style type="text/css">
-div.main { width: 1000px; margin: 0 auto 0 auto; text-align:center;}
-table, td, th { border: 2px #808080 solid;}
-table { margin: 0 auto 0 auto;}
-th { font-family: sans-serif; font-size: 14px;}
-td.contact_info { font-family:sans-serif; font-size:12px;
-font-weight:700; text-align: left;}
-</style>
-</head>
-<body>
-<div class="main">
-<h1>キャリア別 QRコード</h1>
-<table>
-<tr>
-  <th>連絡情報</th>
-  <th>au</th>
-  <th>docomo</th>
-  <th>softbank</th>
-</tr>
-: for $contact_infos -> $contact_info {
-  <tr>
-  <td class="contact_info">
-      <: newline_to_br_tag($contact_info.as_string()) | mark_raw :>
-  </td>
-:  for $carriers.sort() -> $carrier {
-      <td>
-         <img src="data:image/png;base64,<: $contact_info.qrcode_as_base64($carrier) :>"
-              alt="<: $carrier :>_QRcode" />
-      </td>
-:  }
-  </tr>
-: }
-</table>
-</div>
-</body>
-</html>
